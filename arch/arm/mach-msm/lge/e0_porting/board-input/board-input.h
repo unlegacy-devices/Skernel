60825106fb7c587c67999bc0afa8bdd4fcf76007
AirDeeEx <danikdax@gmail.com>
2024-05-01 22:01:46 +0300

    e0: Add button leds support

--- arch/arm/mach-msm/lge/e0_porting/board-input/board-input.h
+++ arch/arm/mach-msm/lge/e0_porting/board-input/board-input.h
@@ -7,6 +7,8 @@ void __init add_gp2ap_proximity(void);
 
 void __init add_melfes_touchscreen(void);
 
-void __init add_gpio_keypad(void); //always available
+void __init add_gpio_keypad(void);
+
+void __init add_pmic_leds(void);
 
 #endif // __ARCH_MSM_BOARD_E0_INPUT_H
--- arch/arm/mach-msm/lge/e0_porting/board-msm7x27a_e0.c
+++ arch/arm/mach-msm/lge/e0_porting/board-msm7x27a_e0.c
@@ -802,6 +802,7 @@ void __init msm7627a_add_io_devices(void)
 	add_gp2ap_proximity();
 	add_melfes_touchscreen();
 	add_gpio_keypad();
+	add_pmic_leds();
 
 	msm_init_pmic_vibrator();
 }
